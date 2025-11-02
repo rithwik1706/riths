3d35849c8afcae0ce57477d86ca324ac5f281732
Rithwik0172005 <rithwikisunuri616@gmail.com>
2025-11-02 11:14:49 +0530

    lo

---
+++ Main.java
@@ -0,0 +1,17 @@
+// Basic Java Program
+public class Main {
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, World!");
+
+        // Declare and initialize variables
+        int a = 10;
+        int b = 20;
+
+        // Perform addition
+        int sum = a + b;
+
+        // Display the result
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
